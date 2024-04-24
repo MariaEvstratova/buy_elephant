220e858ac78edc13766baad9665f31097405dd8a
mariaevstratova <evstratova.mashenka@gmail.com>
2024-04-24 18:42:59 +0700

    release

--- server.py
+++ server.py
@@ -23,14 +23,14 @@ def main():
         }
     }
 
-    handle_dialog(request.json, response)
+    handle_dialog(request.json, response, 'слона')
 
     logging.info(f'Response:  {response!r}')
 
     return jsonify(response)
 
 
-def handle_dialog(req, res):
+def handle_dialog(req, res, text):
     user_id = req['session']['user_id']
 
     if req['session']['new']:
@@ -42,7 +42,7 @@ def handle_dialog(req, res):
             ]
         }
         # Заполняем текст ответа
-        res['response']['text'] = 'Привет! Купи слона!'
+        res['response']['text'] = f'Привет! Купи {text}!'
         # Получим подсказки
         res['response']['buttons'] = get_suggests(user_id)
         return
@@ -51,18 +51,24 @@ def handle_dialog(req, res):
         'ладно',
         'куплю',
         'покупаю',
-        'хорошо'
+        'хорошо',
+        'я покупаю',
+        'я куплю',
+        'я хочу купить'
     ]:
-        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
+        res['response']['text'] = f'{text} можно найти на Яндекс.Маркете!'
         res['response']['end_session'] = True
         return
 
     res['response']['text'] = \
-        f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
-    res['response']['buttons'] = get_suggests(user_id)
+        f"Все говорят '{req['request']['original_utterance']}', а ты купи {text}!"
+    res['response']['buttons'] = get_suggests(user_id, text)[0]
+    res['response']['text'] = 'Купи кролика!'
+    text = get_suggests(user_id, text)[1]
+    handle_dialog(request.json, res, text)
 
 
-def get_suggests(user_id):
+def get_suggests(user_id, text):
     session = sessionStorage[user_id]
 
     suggests = [
@@ -76,11 +82,9 @@ def get_suggests(user_id):
     if len(suggests) < 2:
         suggests.append({
             "title": "Ладно",
-            "url": "https://market.yandex.ru/search?text=слон",
-            "hide": True
+            "url": f"https://market.yandex.ru/search?text={text[:-1]}"
         })
-
-    return suggests
+    return (suggests, "кролика")
 
 
 if __name__ == '__main__':
